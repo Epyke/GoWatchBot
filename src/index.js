94f44003e0558a89c3b519d94a3faeee48b00d0f
Epyke <150194691+Epyke@users.noreply.github.com>
2026-02-07 03:29:37 +0000

    server-subscribe-system

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 require("dotenv").config();
-const { Client, IntentsBitField, ActivityType } = require("discord.js");
+const { Client, IntentsBitField, Partials } = require("discord.js");
 const mongoose = require("mongoose");
 const deepl = require("deepl-node");
 const { CommandKit } = require("commandkit");
 
 const client = new Client({
+  partials: [
+    Partials.Channel,
+    Partials.Message,
+    Partials.User,
+    Partials.GuildMember,
+    Partials.Reaction,
+  ],
   intents: [
     IntentsBitField.Flags.Guilds,
     IntentsBitField.Flags.GuildMembers,
     IntentsBitField.Flags.GuildMessages,
     IntentsBitField.Flags.GuildPresences,
     IntentsBitField.Flags.MessageContent,
+    IntentsBitField.Flags.DirectMessages,
   ],
 });
 
@@ -25,6 +33,7 @@ const client = new Client({
       client,
       eventsPath: `${__dirname}/events`,
       commandsPath: `${__dirname}/commands`,
+      bulkRegister: true,
     });
 
     client.login(process.env.BOT_TOKEN);
